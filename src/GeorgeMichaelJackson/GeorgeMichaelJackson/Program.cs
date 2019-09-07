1bd2e21b62616a5bc7b82180e454520665ac3058
Luke Pothier <lukepothier@gmail.com>
2019-09-07 18:59:32 +0200

    Create project

---
+++ src/GeorgeMichaelJackson/GeorgeMichaelJackson/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GeorgeMichaelJackson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.Read();
+        }
+    }
+}
